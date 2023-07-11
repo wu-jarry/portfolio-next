388cd3917199ad0bddbf44a6c1c50e985e018492
Jarry Wu <jarrywu@Macintosh.local>
2023-07-11 10:06:35 -0400

    first setup

---
+++ src/app/(home–components)/main-canvas.tsx
@@ -0,0 +1,17 @@
+"use client"
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { Box } from './box'
+
+export const MainCanvas = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+      <OrbitControls />
+    </Canvas>
+  )
+}
