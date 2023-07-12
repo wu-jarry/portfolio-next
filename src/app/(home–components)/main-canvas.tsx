5ef4c54ebe469b8d4aa272da574de7ff37959e82
Jeffrey Li <jeffrey.dot.li@gmail.com>
2023-07-12 13:17:27 -0400

    fix: orthographic camera component

--- src/app/(home–components)/main-canvas.tsx
+++ src/app/(home–components)/main-canvas.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, OrthographicCamera as OrthographicCameraComponent } from '@react-three/drei'
 import { Box } from './box'
 import { RoomModel } from './room'
 import { MouseEventHandler, useCallback, useEffect, useRef } from 'react'
@@ -11,17 +11,8 @@ import { useSpring, config } from '@react-spring/three'
 // 
 import { Stage } from '@react-three/drei'
 
-// const orthographicCamera = new OrthographicCamera(-1, 1, 3.5 / 2, -3.5 / 2);
 
-const createOrthographicCamera = (aspectRatio: number) => {
-  const orthographicCamera = new OrthographicCamera((-aspectRatio * 3.5) / 2, (aspectRatio * 3.5) / 2, 3.5 / 2, -3.5 / 2, -50, 50);
-  orthographicCamera.setRotationFromEuler(new Euler(-Math.PI / 6));
-  orthographicCamera.translateY(3);
-  orthographicCamera.translateZ(5);
-  orthographicCamera.zoom = 20;
-  return orthographicCamera;
-}
-
+const cameraRotation = new Euler(-Math.PI / 6)
 
 const calcRotation = (clientX: number) => ((clientX - window.innerWidth / 2) * 0.2) / window.innerWidth;
 const interpolateRotation = (rotateY: number) => [0, rotateY, 0] as const;
@@ -29,12 +20,6 @@ const interpolateRotation = (rotateY: number) => [0, rotateY, 0] as const;
 export const MainCanvas = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const { aspectRatio } = useContainerDimensions(containerRef);
-  const orthographicCamera = useRef(createOrthographicCamera(aspectRatio));
-
-  useEffect(() => {
-    orthographicCamera.current.left = (-aspectRatio * 3.5) / 2;
-    orthographicCamera.current.right = (aspectRatio * 3.5) / 2;
-  }, [aspectRatio])
 
   const [props, set] = useSpring(() => ({
     rotateY: 0,
@@ -50,7 +35,14 @@ export const MainCanvas = () => {
 
   return (
     <div ref={containerRef} className='w-full h-full' onMouseMove={onMouseMove}>
-      <Canvas shadows camera={orthographicCamera.current} >
+      <Canvas shadows >
+        <OrthographicCameraComponent
+          makeDefault
+          args={[(-aspectRatio * 3.5) / 2, (aspectRatio * 3.5) / 2, 3.5 / 2, -3.5 / 2, -50, 50]}
+          rotation={cameraRotation}
+          zoom={20}
+          position={[0, 10, 5]}
+        />
         <SunlightSource />
         <RoomModel rotation={props.rotateY.to(interpolateRotation) as unknown as [number, number, number]} position={[-1, 0, 0]} />
         {/* <OrbitControls ref={orbitControlsRef} /> */}
